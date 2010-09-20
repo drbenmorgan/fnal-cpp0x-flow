0088b10d6de952a019ea21282378f76d0db9ad27
Walter E Brown <wb@fnal.gov>
2010-09-20 09:59:32 -0500

    Initial file import.

---
+++ test/main.cc
@@ -0,0 +1,43 @@
+// ======================================================================
+//
+//
+// ======================================================================
+
+
+#include "algorithm"
+#include "array"
+#include "functional"
+#include "memory"
+#include "numeric"
+#include "regex"
+#include "type_traits"
+
+
+// array
+std::array<int,10>  a;
+
+
+// memory
+std::shared_ptr<int>  sp = std::make_shared<int>( a[9] );
+std::unique_ptr<int>  up( new int(0) );
+
+
+// type_traits
+typedef  std::make_unsigned<int>::type  uint;
+uint u = 0u;
+std::add_lvalue_reference<uint>::type  r = u;
+
+
+// functional
+void v(int,int) { }
+std::function<void(int)>  f( std::bind(v, 0, std::placeholders::_1) );
+
+
+int
+  main( )
+{
+  f(0);
+
+  return u;
+
+}  // main()
