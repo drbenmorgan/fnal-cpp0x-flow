b7115cf5724dcda137057da561770b80f66f7674
Walter E Brown <wb@fnal.gov>
2010-09-30 22:20:25 -0500

    Improve system_error testing.

--- test/system_error_test.cc
+++ test/system_error_test.cc
@@ -6,7 +6,9 @@
 
 
 #include "cpp0x/system_error"
+#include "cpp0x/type_traits"
 #include <cstdlib>
+#include <string>
 
 
 void
@@ -14,6 +16,71 @@ void
 { if( not claim )
     std::exit(which);
 }
+
+
+namespace my_code {
+  enum my_code_t { E1 = 101, E2, E3 };
+}
+
+
+class my_category_impl
+  : public std::error_category
+{
+public:
+  virtual  const char*
+    name( ) const
+  {
+    return "my_category";
+  }
+
+  virtual  std::string
+    message( int ev ) const
+  {
+    switch( ev ) {
+      case my_code::E1: return "E1";
+      case my_code::E2: return "E2";
+      case my_code::E3: return "E3";
+      default         : return "Unknown";
+    };
+  }
+
+};  // my_category_impl
+
+
+std::error_category const &
+  my_category( )
+{
+  static  my_category_impl  my_category;
+  return my_category;
+}
+
+
+std::error_code
+  make_error_code( my_code::my_code_t e )
+{
+  return std::error_code( static_cast<int>(e)
+                        , my_category()
+                        );
+}
+
+
+std::error_condition
+  make_error_condition( my_code::my_code_t e )
+{
+  return std::error_condition( static_cast<int>(e)
+                             , my_category()
+                             );
+}
+
+
+namespace boost { namespace system {  // TODO: how to move this to std?
+
+  template<>
+    struct is_error_code_enum<my_code::my_code_t>
+      : public std::true_type
+  { };
+
+} }
 
 
 int
