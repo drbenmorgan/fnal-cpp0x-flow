4ee0802d1223856b7cbb6aa3f6014c332ad07dd3
Walter E Brown <wb@fnal.gov>
2010-10-19 11:06:54 -0500

    Add config_test.

---
+++ test/config_test.cc
@@ -0,0 +1,27 @@
+// ======================================================================
+//
+// test internal "_config_" header
+//
+// ======================================================================
+
+
+#include "cpp0x/_config_"
+#include <cstdlib>
+
+
+void
+  ensure( int which, bool claim )
+{ if( not claim )
+    std::exit(which);
+}
+
+
+int
+  main( )
+{
+
+  ensure( 1, GCC_IS_AT_LEAST(4,2,0) );
+
+  return 0;
+
+}  // main()
